bed088ae010c064b4b90a49702a51935e1a2f701
MrTast <Mrtast@hotmail.com>
2022-02-14 21:25:44 +0100

    game

---
+++ game.js
@@ -0,0 +1,100 @@
+class Block
+{
+    
+    constructor(x, y, width, height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    render()
+    {
+        fill(255);
+        rect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Player
+{
+    constructor(x, y, width, height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.velX = 0.0;
+    }
+
+    render()
+    {
+        fill(0, 255, 255);
+        rect(this.x, this.y, this.width, this.height);
+    }
+
+    update()
+    {
+        this.x = mouseX - this.width/2;
+        this.x = constrain(this.x, 0, canvaswidth - this.width);
+    }
+}
+
+class Ball
+{
+    constructor(x, y, width)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.velX = 0.0;
+        this.velY = -1.0;
+    }
+
+    render()
+    {
+        fill(0, 255, 255);
+        circle(this.x, this.y, this.width);
+    }
+
+    update()
+    {
+       this.x += this.velX;
+       this.y += this.velY;
+
+       if(this.x - this.width <= 0)
+       {
+           this.velX *= -1;
+       }
+       this.x = constrain(this.x, 0, canvaswidth - this.width);
+    }
+}
+
+function AABB(ball, block)
+{
+    if(ball.x < block.x + block.width
+    && ball.x > block.x
+    && ball.y - (ball.width / 2) < block.y + block.height
+    && ball.y + (ball.width / 2)> block.y)
+    {
+        return true;
+    }
+    return false;
+}
+function AABBplayer(ball, player)
+{
+    if(ball.x - (ball.width / 2) < player.x + player.width
+    && ball.x + (ball.width / 2) > player.x
+    && ball.y - (ball.width / 2) < player.y + player.height
+    && ball.y + (ball.width / 2) > player.y)
+    {
+        diff = Math.abs(ball.x - player.x);
+        return diff;
+    }
+    return false;
+}
+
+
+
+
+
---
+++ setup.js
@@ -0,0 +1,50 @@
+canvaswidth = 480;
+canvasheight = 360;
+
+
+function setup()
+{
+    createCanvas(canvaswidth,canvasheight);
+    blockarray = [];
+    for(var i = 0; i < 4; i++)
+    {
+        for(var j = 0; j < 12; j++)
+        {
+            block = new Block(j * 40, i * 20, 40, 20);
+            blockarray.push(block);
+        }
+    }
+
+    player = new Player((canvaswidth / 2) - 30, canvasheight - 30, 60, 10);
+    ball = new Ball(100, 150, 15);
+}
+
+function draw()
+{
+    background(0);
+    for(var i = 0; i < blockarray.length; i++)
+    {
+        blockarray[i].render();
+        if(AABB(ball, blockarray[i]))
+        {
+            blockarray.splice(i, 1);
+            ball.velY *= -1;
+        }
+    }
+    diff = AABBplayer(ball, player);
+    if(diff)
+    {
+        ball.velY *= -1;
+        ball.velX *= diff;
+    }
+    
+    
+
+    player.render();
+    player.update();
+    ball.render();
+    ball.update();
+   
+}
+
+
